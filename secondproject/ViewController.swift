7db1e90393949a5c8ab46a2b392172c5e8eb60a1
karimamaaoui <maaouika07@gmail.com>
2024-02-25 22:24:10 +0100

    Initial Commit

---
+++ secondproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  secondproject
+//
+//  Created by karimamaaoui on 2/25/24.
+//  Copyright © 2024 karimamaaoui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
