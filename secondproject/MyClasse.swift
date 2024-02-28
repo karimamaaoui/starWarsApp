322278c7513ddf2f7cf8ba182c6617c0fcaa2ae1
karimamaaoui <maaouika07@gmail.com>
2024-02-29 00:42:29 +0100

    "third commit"

---
+++ secondproject/MyClasse.swift
@@ -0,0 +1,13 @@
+//
+//  MyClasse.swift
+//  secondproject
+//
+//  Created by karimamaaoui on 2/29/24.
+//  Copyright © 2024 karimamaaoui. All rights reserved.
+//
+
+import UIKit
+
+class MyClasse: UITableViewCell {
+
+}
